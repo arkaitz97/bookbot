aa39ca24dcd2e57f229f7b09b5dae4079cda3502
Arkaitz Molinuevo Amelivia <arkaitz.molinuevoamelivia@lumen.com>
2025-11-24 18:45:58 +0100

    feat: add function to count occurrences of each letter in the text

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_count_of_each_letter
 def get_book_text(path):
     with open(path) as f:
         return f.read()
@@ -7,5 +7,7 @@ def main():
     book = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(book)
     print(f"Found {num_words} total words")
+    letters = get_count_of_each_letter(book)
+    print(letters)
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,13 @@
 def get_num_words(string):
     return len(string.split())
+
+def get_count_of_each_letter(string):
+    letters = {}
+    text_lowercase = string.lower()
+    for w in text_lowercase.split():
+        for l in w:
+            if l in letters:
+                letters[l] += 1
+            else:
+                letters[l] = 1
+    return letters
