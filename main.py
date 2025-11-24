64e24c73d2c96fc2dc936958c77d4973106ce6bf
Arkaitz Molinuevo Amelivia <arkaitz.molinuevoamelivia@lumen.com>
2025-11-24 18:38:13 +0100

    refactor: replace inline word count function with dedicated get_num_words function

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+from stats import get_num_words
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-def get_words(string):
-    return len(string.split())
 
 def main():
     book = get_book_text("books/frankenstein.txt")
-    num_words = get_words(book)
+    num_words = get_num_words(book)
     print(f"Found {num_words} total words")
 
 main()
