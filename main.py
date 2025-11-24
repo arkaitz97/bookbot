e43f4758f25a99b1cfe5d8de75ba93413b99eb48
Arkaitz Molinuevo Amelivia <arkaitz.molinuevoamelivia@lumen.com>
2025-11-24 19:24:19 +0100

    feat: update main function to accept book path as command line argument

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from stats import get_num_words, get_count_of_each_letter, sort_dict
+import sys
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def main():
-    book = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+    path = sys.argv[1]
+    book = get_book_text(path)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path}...")
     print("----------- Word Count ----------")
     num_words = get_num_words(book)
     print(f"Found {num_words} total words")
